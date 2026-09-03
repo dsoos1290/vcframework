fab3393104fb1bb894ce6df905662b62b0756345
dsoos1290 <d@soos1290.com>
2026-09-03 10:19:09 +0200

    Update db-sample.php

--- private_html/app/config/db-sample.php
+++ private_html/app/config/db-sample.php
@@ -3,5 +3,5 @@ define('DB_HOST', 'localhost');
 define('DB_USER', '');
 define('DB_PASS', '');
 define('DB_NAME', '');
-define('DB_CHAR', 'utf8mb4');
-define('DB_TIME', '+00:00');
+// define('DB_CHAR', 'utf8mb4');
+// define('DB_TIME', '+00:00');
