6c9b0568a0c364602d1f01050feb4e631f25c06d
dsoos1290 <d@soos1290.com>
2026-09-23 23:47:44 +0200

    #1

--- private_html/app/config/app.php
+++ private_html/app/config/app.php
@@ -1,5 +1,6 @@
 <?php
-// define('APP_NAME', 'App');
+// define('APP_NAME', 'VC Framework');
+// define('APP_PROJECT_URL', 'https://github.com/dsoos1290/vcframework');
 define('APP_VER', '0.1.0');
 define('APP_TZ', 'UTC');
 define('APP_ENV', 'dev'); // prod
